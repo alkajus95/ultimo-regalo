8476aff8c1b1494736d0d06888aad55265d1a713
alkajus95 <alejograciacarrillo@gmail.com>
2025-09-21 15:17:17 -0500

    no es mucho

---
+++ anim.js
@@ -0,0 +1,53 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "Cuando se llenó la luna", time: 6 },
+  { text: "vi tu rostro en una estrella", time: 9 },
+  { text: "y en cuanto comprendí lo bella que es", time: 13 },
+  { text: "Sentí yo no estar a la altura", time: 16 },
+  { text: "Como entendí que nadie es perfecto", time: 20 },
+  { text: "por tus carencias de cordura", time: 24 },
+  { text: "Pero es que son tus mismos defectos", time: 27 },
+  { text: "Que te hacen ser una ternura", time: 33 },
+]
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
